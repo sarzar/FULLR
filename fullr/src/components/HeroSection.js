b4ba16b0d2a23f8a240d5649323bc4b1881a9aac
Sarim Zia <username@email.com>
2021-03-05 16:08:03 -0500

    updated card component

--- fullr/src/components/HeroSection.js
+++ fullr/src/components/HeroSection.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import {Button} from './Button';
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
 import './HeroSection.css';
-import "../App.css";
 
 function HeroSection() {
-    return (
-        <div className = "hero-container">
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
-            <h1>FULLR</h1>
-            <p>Help Now!</p>
-            <div className= "hero-btns">
-                <Button className = "btns" buttonStyle = "btn--outline" buttonSize = "btn--large">Sign Up</Button>
-                <Button className = "btns" buttonStyle = "btn--primary" buttonSize = "btn--large">FAQ <i className="fa-play-circle" /> </Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <h1>FULLR</h1>
+      <p>Help stop wastage</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          Order Now
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          FAQ <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
