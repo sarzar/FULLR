6b0ec7fbc5defabd68cc772c1a4641456c72be08
elly-4 <63363643+elly-4@users.noreply.github.com>
2021-03-08 20:31:02 -0500

    Update Signup.js

--- fullr/src/components/pages/Signup.js
+++ fullr/src/components/pages/Signup.js
@@ -4,17 +4,20 @@ import '../Signup.css';
 
 function SignUp () {
     return (
-        <div> 
+        <div style={{ color: '#33333',backgroundColor: '#333333', height: 140 }}> 
+        
             <form >
                 <h1 className = "signUp"> Sign up </h1>
                 <label className = "firstName"> First Name </label> <input type = "text" className = "enterValue"  placeholder = "First Name "/><br />
-                <label > Last Name :</label> <input type = "text" placeholder = "Last Name"/><br />
-                <label> Password :</label> <input type = "text"  placeholder = "Password"/> <br />  
-                <label> Confirm Password :</label> <input type = "text" placeholder = "Confirm Password"/><br />
-                
-                
-               
-               <input type = "submit" value="Submit"/>
+                <label  className = "lastName"> Last Name</label> <input type = "text" className = "enterValue" placeholder = "Last Name"/><br />
+
+                <label className = "address"> Address</label> <input type = "text"  className = "enterValue" placeholder = "Address"/><br />
+                <label className = "postalCode"> Postal Code</label> <input type = "text"  className = "enterValue" placeholder = "Postal Code"/><br />
+                <label className = "emailAddress"> Email Address</label> <input type = "text"  className = "enterValue" placeholder = "Email Address"/><br />
+                <label className = "phoneNumber"> Phone Number</label> <input type = "text"  className = "enterValue" placeholder = "Phone Number"/><br />
+                <label  className = "password"> Password</label> <input type = "text"  className = "enterValue" placeholder = "Password"/> <br />  
+                <label className = "confirmPassword"> Confirm Password</label> <input type = "text"  className = "enterValue" placeholder = "Confirm Password"/><br />
+               <input  className = "submitButton" type = "submit" value="Submit"/>
             </form>
 
 
@@ -27,4 +30,8 @@ function SignUp () {
 
 
 }
-export default SignUp;
+
+
+
+
+export default SignUp
